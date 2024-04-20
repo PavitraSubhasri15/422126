d74f4c98d5bd94866efc67b6c2678ab4a24509e7
Pavitra Subhasri <141206259+PavitraSubhasri15@users.noreply.github.com>
2024-04-20 20:32:24 +0530

    Create multiple.sh

---
+++ task7/multiple.sh
@@ -0,0 +1,16 @@
+ machines=("172.50.4.179" ,"172.50.2.188") 
+ 
+# SSH user fetch_memory_usage(){ 
+    ssh $1 "free -m" 
+} 
+fetch_cpu_usage(){   
+ssh $1 "top -b -n 1| grep 'Cpu(s)' " 
+} 
+ 
+# Loop through machines and get usage info for machine in "${machines[@]}"; do
+echo "Memory usage on $machine:"   
+fetch_memory_usage $machine    
+echo "CPU usage on $machine:"   
+fetch_cpu_usage $machine 
+    echo "-------------------------------------------" 
+done 
