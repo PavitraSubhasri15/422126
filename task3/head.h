59b2e7ecdba1c648da86a08537457ac10a1cfa76
Pavitra Subhasri <141206259+PavitraSubhasri15@users.noreply.github.com>
2025-04-03 22:59:48 +0530

    Delete task3 directory

--- task3/head.h
+++
@@ -1,3 +0,0 @@
-int *composite(int n, int *primeCount);
-bool isPerfectSquare(int num);
-int linearSearchPerfectSquares(int *arr, int size);
--- task3/perfect.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <math.h>
-bool isPerfectSquare(int num) {
- double root = sqrt(num);
- return root == floor(root); // Check if square root is an integer
-}
